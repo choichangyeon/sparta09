7c631760e0e490db2ed03974d50fbff001603292
choichangyeon <changyon99@naver.com>
2025-01-07 21:23:15 +0900

    fixed file sturcture

--- solution2.js
+++
@@ -1,9 +0,0 @@
-const changyeon = {
-    name: "최창연",
-    age: 26,
-    mbti: "ISTP"
-};
-
-console.log(changyeon.name);
-console.log(changyeon.age);
-console.log(changyeon.mbti);
--- solution3.js
+++
@@ -1,6 +0,0 @@
-function oddOrEven(a){
-    return a&1? "홀수":"짝수";
-}
-  
-console.log(oddOrEven(10)); // 결과값 "짝수";
-console.log(oddOrEven(7)); // 결과값 "홀수";
